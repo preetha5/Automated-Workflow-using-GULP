1a352c348e03137be5882834d849d2cb79f85a8b
Preetha Ramakrishnan <preetha_ramki@hotmail.com>
2017-09-20 13:29:04 -0700

    added compass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
     browserify = require('gulp-browserify');
 
 var coffee_src = ['components/coffee/tagline.coffee'];
@@ -10,6 +11,8 @@ var js_src = ['components/scripts/rclick.js',
               'components/scripts/tagline.js',
               'components/scripts/template.js',
              ];
+
+var sass_src = ['components/sass/styles.scss']
 
 gulp.task('coffee', function(){
     gulp.src(coffee_src)
@@ -23,4 +26,15 @@ gulp.task('js', function(){
     .pipe(concat('scripts.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+    gulp.src(sass_src)
+    .pipe(compass({
+        sass:'components/sass',
+        image:'builds/development/images',
+        style:'expanded'
+    }))
+    .on('error',gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
