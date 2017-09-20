12515ffd0f2ae6d9cfadfd0e4834aa442beb2ca2
Preetha Ramakrishnan <preetha_ramki@hotmail.com>
2017-09-19 22:02:07 -0700

    concatentated JS files using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffee_src = ['components/coffee/tagline.coffee'];
+var js_src = ['components/scripts/rclick.js',
+             'components/scripts/pixgrid.js',
+              'components/scripts/tagline.js',
+              'components/scripts/template.js',
+             ];
+
 gulp.task('coffee', function(){
     gulp.src(coffee_src)
     .pipe(coffee({bare: true})
         .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts')) 
+});
+
+gulp.task('js', function(){
+    gulp.src(js_src)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
